db3d5708b68e86e531303502a10c439866529397
Yogesh Chandra <002chandra.yogesh@gmail.com>
2022-07-01 00:31:01 +0530

    updating navbar

--- src/Components/Header/Index.js
+++ src/Components/Header/Index.js
@@ -2,28 +2,39 @@ import React, { useState } from "react";
 import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import logo1 from "../../Assets/images/logo-1.png";
 import logo2 from "../../Assets/images/logo-2.png";
+import "./index.css"
 
 const Header = () => {
 
   const [logo, setLogo] = useState(logo2);
+  const [width, setWidth] = useState("180");
+  const [height, setHeight] = useState("80");
+  const [bg, setbg] = useState("");
   const changeNavbarLogo = () =>{
      if(window.scrollY >= 80){
        setLogo(logo2);
+       setWidth("160");
+       setHeight("50");
+       setbg("bg-grey")
+
      }
      else{
        setLogo(logo1);
+       setWidth("180")
+       setHeight("80")
+       setbg("")
      }
   };
   window.addEventListener('scroll', changeNavbarLogo);
 
   return (
-    <Navbar collapseOnSelect expand="lg" fixed="top" className="navbar">
+    <Navbar collapseOnSelect expand="lg" fixed="top" className={`navbar ${bg}`}>
       <Container>
         <Navbar.Brand href="#home">
           <img
             src={logo}
-            width="180"
-            height="80"
+            width = {width}
+            height={ height}
             className="d-inline-block align-top"
             alt="React Bootstrap logo"
           />
@@ -33,17 +44,17 @@ const Header = () => {
           id="responsive-navbar-nav"
           className="justify-content-end"
         >
-          <Nav className="align-items-center">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link eventKey={2} href="/about">
+          <Nav className="align-items-center nav-items">
+            <Nav.Link className="mx-2 nav-links" href="/">Home</Nav.Link>
+            <Nav.Link className="nav-links" eventKey={2} href="/about">
               About us
             </Nav.Link>
-            <Nav.Link eventKey={2} href="/contact">
+            <Nav.Link className="nav-links" eventKey={2} href="/contact">
               Contact us
             </Nav.Link>
 
             <Nav.Link href="/booking">
-              <Button variant="primary">Book Now</Button>
+              <Button className="book-now-btn">Book Now</Button>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
