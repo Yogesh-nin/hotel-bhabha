bae40f6b933ac160cafec32af2f5a26ffc6e4835
navdeep1840 <navikitu001@gmail.com>
2022-07-01 11:16:51 +0530

    map-added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,17 +4,14 @@ import React from "react";
 import Header from "../Components/Header/Index";
 import Slider from "../Components/Slider/Slider";
 import Footer from "../Components/Footer";
+import MapView from "../Components/Map.jsx";
 
 function Home() {
   return (
     <div>
       <Header />
       <Slider />
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam</h1>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam</h1>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam</h1>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam</h1>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam</h1>
+      <MapView />
       <Footer />
     </div>
   );
